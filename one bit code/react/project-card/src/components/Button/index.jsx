b4cefd7f67f32a0528163fa4398f829f3ff77568
viniciusFujarra <vinicius.souza@numenit.com>
2026-06-22 17:43:52 -0300

    feat: add Card and Button components with styles, global styles, and Vite configuration
    - Created a new SVG file for Vite logo.
    - Implemented Button component with styles for a purchase button.
    - Developed Card component to display a poster with title and description, including the Button component.
    - Added global styles for consistent styling across the application.
    - Configured Vite for the project setup.

---
+++ one bit code/react/project-card/src/components/Button/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.css"
+export default function Card() {
+  return (
+    <div>
+      <button className={styles.button}>Comprar agora</button>
+    </div>
+  )
+}
+
